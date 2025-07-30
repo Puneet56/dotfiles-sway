9fa6256a4bcf334b813f37be5dcded5a744f6a9e
Puneet56 <puneetdhiman56@gmail.com>
2025-07-30 21:54:19 +0530

    update

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,3 +1,10 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.writebackup = false
+vim.opt.undofile = true
+vim.opt.timeoutlen = 500
+vim.opt.updatetime = 50
